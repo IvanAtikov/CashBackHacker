1d4611d542b57b6d401ee0c100e949e711d1e7a2
Ivan Atikov <atikov-ia@yandex.ru>
2023-08-07 22:07:16 +0700

    Final correcting JUnit 4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -11,22 +11,22 @@ public class CashbackHackServiceTest {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(1900);
         int expected = 100;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
     @Test
     public void lessThanBoundary() {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(800);
         int expected = 200;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
     public void equalsBoundary() {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(1000);
-        int expected = 1000;
-        assertEquals(actual, expected);
+        int expected = 0;
+        assertEquals(expected, actual);
     }
 
 }
